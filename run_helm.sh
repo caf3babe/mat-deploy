749af2b3d7ffc03de8c182a31ab01db4f4f75199
caf3babe <https://github.com/caf3babe>
2023-02-07 17:50:43 +0100

    fixups for helm charts

--- run_helm.sh
+++ run_helm.sh
@@ -16,9 +16,22 @@ for DIR in ./* ; do
   
 done
 
-helm install --atomic --create-namespace --namespace tracing zipkin ./zipkin # distributed tracing
-helm install --atomic --create-namespace --namespace messaging rabbitmq ./rabbitmq # messaging
-helm install --atomic --create-namespace --namespace logging logstash ./logstash # from elk stack
-helm install --atomic --create-namespace --namespace monitoring kube-prometheus-stack ./kube-prometheus-stack 
-helm install --atomic --create-namespace --namespace cert-manager ./cert-manager 
-helm upgrade -i --atomic --create-namespace --namespace apps --timeout 15m0s car-rental ./car-rental
+
+helm repo add ygqygq2 https://ygqygq2.github.io/charts/
+helm upgrade -i --atomic --create-namespace --namespace tracing -f ./zipkin/values.yaml zipkin ygqygq2/zipkin 
+# zipkin helm chart needs a patch to work with elasticsearch, provide the correct servicename of elasticsearch node as ES_HOSTS variable in deploymentset
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+kubectl create namespace messaging
+kubectl apply -f ./rabbitmq/secret.yaml --namespace messaging 
+helm upgrade -i --atomic --create-namespace --namespace messaging -f ./rabbitmq/values.yaml rabbitmq bitnami/rabbitmq # messaging
+
+helm repo add elastic https://helm.elastic.co
+
+helm upgrade -i --atomic --create-namespace --namespace elastic -f ./elasticsearch/values.yaml elasticsearch elastic/elasticsearch 
+helm upgrade -i --atomic --create-namespace --namespace elastic -f ./kibana/values.yaml kibana elastic/kibana 
+helm upgrade -i --atomic --create-namespace --namespace elastic -f ./logstash/values.yaml logstash elastic/logstash 
+
+helm upgrade -i --atomic --create-namespace --namespace monitoring kube-prometheus-stack ./kube-prometheus-stack 
+helm upgrade -i --atomic --create-namespace --namespace cert-manager ./cert-manager 
+helm upgrade -i --atomic --create-namespace --namespace apps car-rental ./car-rental
