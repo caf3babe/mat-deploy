fb1de449b51e2cd54fafab8d613b5ef76f453901
caf3babe <https://github.com/caf3babe>
2023-02-11 15:21:12 +0100

    config changes

--- run_helm.sh
+++ run_helm.sh
@@ -16,21 +16,16 @@ for DIR in ./* ; do
   
 done
 
-
-helm repo add ygqygq2 https://ygqygq2.github.io/charts/
-helm upgrade -i --atomic --create-namespace --namespace tracing -f ./zipkin/values.yaml zipkin ygqygq2/zipkin 
-# zipkin helm chart needs a patch to work with elasticsearch, provide the correct servicename of elasticsearch node as ES_HOSTS variable in deploymentset
-
 helm repo add bitnami https://charts.bitnami.com/bitnami
 kubectl create namespace messaging
 kubectl apply -f ./rabbitmq/secret.yaml --namespace messaging 
-helm upgrade -i --atomic --create-namespace --namespace messaging -f ./rabbitmq/values.yaml rabbitmq bitnami/rabbitmq # messaging
+helm upgrade -i --atomic --create-namespace --namespace messaging -f ./rabbitmq/values.yaml rabbitmq bitnami/rabbitmq --version 11.9.0
 
-helm repo add elastic https://helm.elastic.co
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm upgrade -i --atomic --create-namespace --namespace monitoring -f ./kube-prometheus-stack/values.yaml kube-prometheus-stack prometheus-community/kube-prometheus-stack --version 45.0.0
 
-helm upgrade -i --atomic --create-namespace --namespace elastic -f ./elasticsearch/values.yaml elasticsearch elastic/elasticsearch 
-helm upgrade -i --atomic --create-namespace --namespace elastic -f ./kibana/values.yaml kibana elastic/kibana 
-helm upgrade -i --atomic --create-namespace --namespace elastic -f ./logstash/values.yaml logstash elastic/logstash 
+helm repo add cert-manager https://charts.jetstack.io
+helm upgrade -i --atomic --create-namespace --namespace cert-manager -f ./cert-manager/values.yaml cert-manager/cert-manager --version 1.11.0
 
-helm upgrade -i --atomic --create-namespace --namespace monitoring kube-prometheus-stack ./kube-prometheus-stack 
-helm upgrade -i --atomic --create-namespace --namespace cert-manager ./cert-manager 
+helm repo add grafana https://grafana.github.io/helm-charts
+helm upgrade -i --atomic --create-namespace --namespace logging -f ./loki/values.yaml loki grafana/loki --version 4.6.0
